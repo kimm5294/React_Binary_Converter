f5c82220dac34af938e3dfb76e2a05ed26e3a6e2
Matthew Kim <kimm5294@gmail.com>
2017-05-16 11:10:48 -0400

    intial commit for translator parent class

---
+++ src/Translator.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+class Translator extends Component {
+
+  constructor(args) {
+    this.state = {
+      numberToConvert: 0;
+    };
+
+  }
+
+  convertToBinary() {
+
+  }
+
+  convertToOctal() {
+
+  }
+
+  convertToHexa() {
+
+  }
+
+  render() {
+    return(
+
+    );
+  }
+
+}
+
+
+
+
+
+
+
+
+
+end
+
+export default Translator
