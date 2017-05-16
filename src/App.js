e3b8ae19a5c895c439476b4f76cafb2b05d22d26
Matthew Kim <kimm5294@gmail.com>
2017-05-16 11:10:38 -0400

    import translator component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './Translator.js'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <section>
+        <div className="App">
+          <h1>Number Converter</h1>
+          <input type="number" id="number-input" />
+          <input type="submit" id="submit-button" value="Convert" />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+
+        <div className="Conversions">
+          <Translator />
+        </div>
+      </section>
     );
   }
 }
